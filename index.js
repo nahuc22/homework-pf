619f78df1c55b8a52fbbd5c0c023304e9bc3b897
Dalejandro31 <74673610+Dalejandro31@users.noreply.github.com>
2023-06-20 15:37:27 -0500

    linea 17

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function laCajaDePandora(numero){
 }
 
 
-console.log(laCajaDePandora(10));  // Salida: "1010" (binario)
+console.log(laCajaDePandora(20));  // Salida: "1010" (binario)
 console.log(laCajaDePandora(15));  // Salida: "f" (hexadecimal)
 
 //Hola soy David
